732a761ef909b33d89909c3295abb3637e42a680
vinhtantran <vinhtantran@gmail.com>
2019-09-01 11:00:19 -0600

    fix copy all contents

--- R/scrs.R
+++ R/scrs.R
@@ -4,9 +4,9 @@ scrs_bookdown <- function(path) {
     # copy 'resources' folder to path
     resources <- system.file("rstudio", "templates", "project", "resources",
                             package = "scrsreport", mustWork = TRUE)
-    files <- list.files(resources, recursive = TRUE, include.dirs = TRUE)
+    files <- list.files(resources, recursive = FALSE, include.dirs = TRUE)
     source <- file.path(resources, files)
-    target <- file.path(path, files)
+    target <- file.path(path)
     file.copy(source, target, recursive = TRUE)
     # add book_filename to _bookdown.yml and default to the base path name
     f <- file.path(path, "_bookdown.yml")
