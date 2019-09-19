2b1573b9b17726909d7bcaca0bd5d8e9663ec5d2
vinhtantran <vinhtantran@gmail.com>
2019-09-19 10:45:53 -0600

    usethis::use_package_doc

---
+++ R/scrsreport-package.R
@@ -0,0 +1,13 @@
+#' SCRS Statistical Report Bookdown Project Template
+#'
+#' This package adds a project template to RStudio. It uses bookdown,
+#' together with current SCRS LaTeX and Markdown templates to facilitate a
+#' professtional looking statistical report.
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
